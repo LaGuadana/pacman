c750a55ab14244f2340e6f863c3ab9c09c04cf7d
La Guadaña <willydaxue@gmail.com>
2026-05-05 00:13:45 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pacman-multiplayer",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "pygame>=2.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pacman-server=server.main:main",
+            "pacman-client=client.main:main",
+        ],
+    },
+    python_requires=">=3.7",
